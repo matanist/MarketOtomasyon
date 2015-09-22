043fab8d81f78cc009c465eec4ed2d022a55029d
matanist <matanist@hotmail.com>
2015-09-22 19:06:52 +0300

    Kategori kısmı tamam

---
+++ Market_Otomasyon/Class1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market_Otomasyon
+{
+    #region Urun tipimiz
+    public class Urun
+    {
+        //public float AlisFiyati { get; set; }
+        //public int Miktari { get; set; }
+        //public Kategori Kategori { get; set; }
+
+        private float _alisFiyati;
+        private int _miktari;
+        private Kategori _kategori;
+        private string _urunAdi;
+        private int _urunID;
+        public Urun() { } //0 parametreli (argümanlı) yapıcı metot (constructor), miras işlemlerinde gerekli. 
+
+        public float AlisFiyati  //Encapsulation(sarmalama=gizleme) işlemleri
+        {
+            get { return _alisFiyati; }
+            set { _alisFiyati = value; }
+        }
+        public int Miktari  //Encapsulation(sarmalama=gizleme) işlemleri
+        {
+            get { return _miktari; }
+            set { _miktari = value; }
+        }
+        public Kategori Kategori //Encapsulation(sarmalama=gizleme) işlemleri
+        {
+            get { return _kategori; }
+            set { _kategori = value; }
+        }
+        public string UrunAdi //Encapsulation(sarmalama=gizleme) işlemleri
+        {
+            get { return _urunAdi; }
+            set { _urunAdi = value; }
+        }
+        public int UrunID //Encapsulation(sarmalama=gizleme) işlemleri
+        {
+            get { return _urunID; }
+            set { _urunID = value; }
+        }
+
+
+        public Urun(Kategori pKategori, int pMiktari, float pAlisFiyati, string pUrunAdi, int pUrunID)
+        {
+
+            //Aşağıda yaptığımız işlemin şu an için bir gereği yok. Ancak GetType() metodu ve typeof() için örnek olması açısından yazıyorum.
+            //Biz değerleri girerken zaten farklı bir tipte değer giremeyeceğimizden gerekli değil. 
+            if (pKategori.GetType() == typeof(Kategori))  //parametre olarak gelen pKategori'nin tipi Kategori tipinde ise eşitle. 
+                Kategori = pKategori;
+            else
+                Kategori = new Kategori("isimsiz Kategori", 0); //değilse bir kategori oluştur. 
+
+            if (pMiktari.GetType() == typeof(int)) //pMiktari'nin tipi int ise atama yap yoksa 0'a eşitle. 
+                Miktari = pMiktari;
+            else
+                Miktari = 0;
+
+            if (pAlisFiyati.GetType() == typeof(float))  //pAlisMiktari'nin tipi float ise kabul et değilse 0f'e eşitle. 
+                AlisFiyati = pAlisFiyati;
+            else
+                AlisFiyati = 0f;  //0f'den kasıt bu sayının bir float tipte olduğunu belirtir. 0 yazsak da bir sorun olmazdı, kendisi otomatik çevirirdi. 
+            //C#'da küçük türler büyük türlere otomatik olarak çevrilir. int<float (tip olarak)
+
+            _urunAdi = pUrunAdi;  //Tip kontrolü gerçekleştirmedim. 
+            _urunID = pUrunID;
+        }
+        public virtual float KdvHesapla()  //KDV hesaplayan metodumuz, polimorfizm için. 
+        {
+
+            return _alisFiyati * (18 / 100); //Varsayılan olarak ürünlerin KDV'sini %18 olarak hesaplıyor. 
+        }
+    }
+    #endregion
+
+    #region Urun'lerin kategorisini tutan tip.
+    public class Kategori
+    {
+        public string KategoriAdi { get; set; }
+        public int KategoriID { get; set; }
+
+        public Kategori(string pKategoriAdi, int pKategoriID)
+        {
+            if (pKategoriAdi.GetType() == typeof(string))
+                KategoriAdi = pKategoriAdi;
+            else
+                KategoriAdi = pKategoriAdi.ToString();  //Parametre olarak gelen KategoriAdi string'den farklı ise stringe çevir. 
+
+            KategoriID = pKategoriID; //Bu da yukarıdakinden farklı olarak kontrolsüz olarak atanıyor. Bu iki yöntemden birinin yapılmaması durumunda, kategori id'si sürekli kendi varsayılan(default) değeri atanacak (0). 
+            //constructor içinde mutlaka sınıf propertilerine atama yapılmalıdır, yoksa hatalarla karşılaşmak olası. 
+        }
+
+    }
+    #endregion
+
+    #region Yenebilir Ürünler için tip
+    public class YenebilirUrun : Urun
+    {
+        public YenebilirUrun() { }  //Miras için gerekli. 
+
+        public YenebilirUrun(Kategori pKategori, int pMiktari, float pAlisFiyati, string pUrunAdi, int pUrunID)
+        {
+            base.AlisFiyati = pAlisFiyati;
+            base.Kategori = pKategori;
+            base.Miktari = pMiktari;
+            base.UrunAdi = pUrunAdi;
+            base.UrunID = pUrunID;
+
+        }
+
+        public override float KdvHesapla()
+        {
+            return base.AlisFiyati/100*8;
+        }
+    }
+    #endregion
+    #region Diğer Ürünler için tip
+    public class DigerUrun:Urun
+    {
+        public DigerUrun(){}
+        public DigerUrun(Kategori pKategori, int pMiktari, float pAlisFiyati, string pUrunAdi, int pUrunID)
+        {
+            base.AlisFiyati = pAlisFiyati;
+            base.Kategori = pKategori;
+            base.Miktari = pMiktari;
+            base.UrunAdi = pUrunAdi;
+            base.UrunID = pUrunID;
+        }
+        public override float KdvHesapla()
+        {
+            return base.AlisFiyati/100*18;
+        }
+    }
+    #endregion
+
+}
---
+++ Odev_ConsoleApplication/Program.cs
@@ -0,0 +1,131 @@
+﻿using Market_Otomasyon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_ConsoleApplication
+{
+    class Program
+    {
+        static YenebilirUrun[] yenebilirUrunlerim = new YenebilirUrun[20];  //static erişim belirleyicisini şu amaçla kullandık: Sınıf üyesine direct erişim için 
+        static DigerUrun[] digerUrunlerim = new DigerUrun[20];  //böylelikle static kullanarak bir sınıf üyesine, sınıfın yeni instanse'ı (örneği) alınmadan erişilebilir.
+        static Kategori[] kategorilerim = new Kategori[20];  //KategorileriGetir() adlı metotta yapıldığı gibi. 
+        static void Main(string[] args)
+        {
+            //Kategori meyveKategorisi = new Kategori("Mevye", 1);
+            //Kategori tekelKategorisi = new Kategori("Sigara", 2);
+            //YenebilirUrun yeniUrun = new YenebilirUrun(meyveKategorisi, 15, 15.4f, "Muz", 1); //float olan parametre için sonuna f eklemek zorundayız(onun float olduğunu belirtmiş olduk) . Varsayılan (default) olarak noktalı bir sayı double türündedir ve double floattan daha büyük bir tiptir (double>float). Bu yüzden kendisi otomatik tür dönüşümü yapamaz. 
+            //DigerUrun digerYeniUrun = new DigerUrun(tekelKategorisi, 20, 9, "Marlboro", 2);
+            //Console.WriteLine(yeniUrun.AlisFiyati + " KDV :" + yeniUrun.KdvHesapla() + " " + yeniUrun.Kategori.KategoriAdi + " " + yeniUrun.UrunID);
+            //Console.WriteLine(digerYeniUrun.AlisFiyati + " KDV :" + digerYeniUrun.KdvHesapla());
+
+            do
+            {
+                Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :\nYeni Eklemek için(y), Mevcutları Görüntülemek için (g), Çıkış için (q)"); //Ana menü seçenekleri
+                var tercih = Console.ReadLine();
+                if (tercih == "q") break; //q ya basılmışsa çık. 
+                else if (tercih == "y") //y'ye basılmışsa yeni giriş ekranına git. 
+                {
+                    var icDongu = true;
+                    while (icDongu)
+                    {
+                        Console.WriteLine("Yeni Kategori oluşturmak için (k), Yeni Ürün oluşturmak için (u), bu ekrandan çıkmak için (q)");
+                        var girilen = Console.ReadLine();
+                        switch (girilen)
+                        {
+                            case "k": //Yeni kategori oluşturulup kategori dizimize ekleniyor.
+                                KategorileriGetir();
+                                YeniKategoriEkle();
+                                break;
+                            case "u": //Yeni ürün oluşturulup ürün dizimize ekleniyor. 
+
+                                break;
+                            case "q":
+                                icDongu = false;
+                                break;
+                            default:
+                                Console.WriteLine("Lütfen doğru bir tuşa basınız...");
+                                break;
+                        }
+
+                    }
+
+
+                }
+                else if (tercih == "g") //g'ye basılmışsa oluşturulmuşları göster. 
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış bir tuşa bastınız, Lütfen tekrar deneyin");
+                    Console.Clear();
+                    continue;
+                }
+            } while (true);
+
+
+            Console.ReadLine();
+        }
+
+        private static void YeniKategoriEkle()
+        {
+            Console.Write("Lütfen KategoriID giriniz :");
+            try
+            {
+                var girilenKategoriID = Convert.ToInt32(Console.ReadLine());  
+                foreach (Kategori kategori in kategorilerim)  //hata oluşmamışsa kategoriID kontrolü yapılıyor.
+                {
+                    if(kategori!=null) //sıradaki kategori eğer boş ise aşağıdaki hata vereceğinden boş değilse devam etsin istedik. 
+                    if (girilenKategoriID == kategori.KategoriID)
+                    {
+                        Console.WriteLine("Girmeye çalıştığınız ID, {0} kategorisi için daha önce tanımlanmış\nLütfen benzersiz bir ID girin", kategori.KategoriAdi);
+                        return; //bu return'ün amacı YeniKategoriEkle() metotundan çıkış yapmak için kullanıldı. Geri dönüş değeri olmayan metotlardan bu şekilde çıkılabilir. 
+                    }
+                }
+                //Buraya kadar herhangi bir sorun yoksa (catch bloğuna girilmediyse veya kategori id farklı ise), buradan sonra yeni kategori ekleme işlemi yapılabilir. 
+                int bostakiIlkIndex = -1;
+                for (int i = 0; i < kategorilerim.Length; i++)
+                {
+                    if (kategorilerim[i]==null) //kategorilerim dizisindeki i numaralı index boş ise bostakiIlkIndex degerine i'yi ata. 
+                    {
+                        bostakiIlkIndex = i;
+                        break; //buradan sonra döngünün çalışmasına artık gerek yok. 
+                    }
+                }
+                if (bostakiIlkIndex==-1) //Bu değer hala böyle kalmış ise dizide hiç boşyer kalmamış demektir. 
+                {
+                    Console.WriteLine("Kategorilerde hiç boş yeriniz kalmamış");
+                    return; //bu ise YeniKategoriEkle() metotundan çıkmayı sağlayacak. 
+                }
+                //Bu satıra gelmeyi başarmışsak bostakiIlkIndex -1'den farklıdır (yani kategorilerde boş index mevcut=boş yer mevcut). 
+                Console.Write("Lütfen Kategori Adı giriniz :");
+                var girilenKategoriAdi = Console.ReadLine();
+                kategorilerim[bostakiIlkIndex] = new Kategori(girilenKategoriAdi, girilenKategoriID);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Lütfen bir tam sayı girişi yapın");
+            }
+            
+            
+        }
+
+        private static void KategorileriGetir() //static erişim belirleyicisi burada da kullanıldı. 
+        {
+            
+                foreach (Kategori kategori in kategorilerim)
+                {
+                    if (kategori == null) //sıradaki kategori eğer boş ise bunu atlıyoruz. 
+                        continue;
+                    //Tüm dizi elemanları boş ise zaten aşağıya inmek mümkün olmayacağından herhangi bir NullException hatası almayacağız. 
+                    Console.WriteLine("Kategori ID\tKategori Adı");
+                    Console.WriteLine(kategori.KategoriID + "\t\t" + kategori.KategoriAdi);
+                }
+           
+        }
+    }
+}
